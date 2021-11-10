af1cb8381b9cda061e959d7685f41b04047865c6
dikeone <93767297+dikeone@users.noreply.github.com>
2021-11-10 09:34:18 +0100

    Add files via upload
    all our projects

---
+++ David/js/index.js
@@ -0,0 +1,20 @@
+const blogs = [
+    { 
+        title1: "hhhhhhhhh",
+        snippet: "jjjjjjjjjjjjj",
+        body: "kkkkkkkkkkkkkkkkkkkkkkkk"
+    },
+    {
+        title2: "lllllllll",
+        snippet: "zzzzzzzzzzzz",
+        body: "xxxxxxxxxxxxxxxxxxxx"
+    },
+    {
+        title3: "cccccccccccccccc",
+        snippet: "vvvvvvvvvvvvvvvvvvv",
+        body: "bbbbbbbbbbbbbbbbbbbbbbbbbbb"
+    }
+]
+
+console.log(blogs[2].snipp
+    et)
